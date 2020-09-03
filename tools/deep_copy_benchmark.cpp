e03d20e277ac915cd6d3f4d0cbe469d29915cd9c
Richard Bailey <rsjtaylor.github@gmail.com>
2020-09-03 13:53:33 +0100

    add deep copy benchmark

--- tools/deep_copy_benchmark.cpp
+++ tools/deep_copy_benchmark.cpp
@@ -1,3 +1,21 @@
-//
-// Created by Richard Taylor on 03/09/2020.
-//
+#include <boost/optional.hpp>
+#include <adm/adm.hpp>
+#include <adm/utilities/object_creation.hpp>
+#include <chrono>
+#include <iostream>
+
+int main () {
+  auto doc = adm::Document::create();
+  for(auto i = 0; i != 500; ++i) {
+    auto holder = adm::createSimpleObject(std::string(std::to_string(i)));
+    doc->add(holder.audioObject);
+  }
+  std::cout << "starting bench" << std::endl;
+  auto startPoint = std::chrono::high_resolution_clock::now();
+  auto copy = doc->deepCopy();
+  auto endPoint = std::chrono::high_resolution_clock::now();
+  auto duration = endPoint - startPoint;
+  std::cout << std::chrono::nanoseconds(duration).count() << std::endl;
+  std::cout << copy->getElements<adm::AudioObject>().size() << '\n'; // just to stop it getting optimised out
+  return 0;
+}
